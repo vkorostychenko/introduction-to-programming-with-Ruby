3f88cc48fbf1f4b28a0f1c80205efeeb00ca2521
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-03-28 21:39:43 +0200

    Done exercises, Methods

---
+++ Methods/exercise_3.rb
@@ -0,0 +1,5 @@
+def multiply(num_1, num_2)
+  num_1 * num_2
+end
+
+puts multiply(5, 6)
---
+++ Methods/exercise_5.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+p scream("Yippeee")
+
+# returns nil 
